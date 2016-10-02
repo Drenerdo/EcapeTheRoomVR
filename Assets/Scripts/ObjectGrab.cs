d117bbc291dbaad2828545e2c0651513ca4ceb99
Dre Smith <dresmith@Dres-MacBook-Pro.local>
2016-10-02 14:21:51 -0400

    Initial commit

---
+++ Assets/Scripts/ObjectGrab.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ObjectGrab : MonoBehaviour 
+{
+	public GameObject puzzle;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Puzzle") 
+		{
+			
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.gameObject.tag == "Puzzle") 
+		{
+			
+		}
+	}
+}
